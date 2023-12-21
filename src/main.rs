be1f78247788a586ff8105a9fe37ef2983d6611d
TheBharatMishra <mishrabm2004@gmail.com>
2023-12-21 13:05:11 +0530

    Ignored

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[allow(unused)]
 
 use std::io;
-use rand::Rng;
-use std::io::{BufReader,BufRead,ErrorKind,Write};
-use std::fs::File;
-use std::cmp::Ordering;
+// use rand::Rng;
+// use std::io::{BufReader,BufRead,ErrorKind,Write};
+// use std::fs::File;
+// use std::cmp::Ordering;
 
 fn main() {
     // println!("What is yout name?");
@@ -15,7 +15,7 @@ fn main() {
     // .expect("We didnt recieve any input");
     // println!("Hello, {}! ,{}",someName.trim_end(),greeting);
     const ONE_MIL :u32=1_000_000;
-    const PI:f32= 3.141592;
+    // const PI:f32= 3.141592;
     let age: &str="47";
     let mut age : u32 = age.trim().parse()
         .expect("Age wasnt assigned a number");
